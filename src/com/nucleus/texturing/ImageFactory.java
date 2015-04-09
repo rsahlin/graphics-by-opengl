01cc0eb0e32c3351cc526743ae7fe1360ee25f37
Richard Sahlin <richard_sahlin@yahoo.com>
2015-04-09 08:15:19 +0200

    Support for creating scaled image in ImageFactory interface

--- src/com/nucleus/texturing/ImageFactory.java
+++ src/com/nucleus/texturing/ImageFactory.java
@@ -13,9 +13,12 @@ public interface ImageFactory {
     /**
      * Loads an image, the image must be in a format that is understood by the platform.
      * 
-     * @return The image The loaded image.
+     * @param name The filename to load
+     * @param scaleX If image should be scaled in X, 1 = original size, 1.5 = 50% bigger
+     * @param scaleY If image should be scaled in Y, 1 = original size, 1.5 = 50% bigger
+     * @return The loaded image.
      * @throws IOException If there is an error loading the image.
      */
-    public Image createImage(String name) throws IOException;
+    public Image createImage(String name, float scaleX, float scaleY) throws IOException;
 
 }
