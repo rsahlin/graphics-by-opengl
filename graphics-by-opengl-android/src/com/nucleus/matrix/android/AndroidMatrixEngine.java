d6d51738b157f2f6be109bbba866cafe67436d63
Richard Sahlin <richard_sahlin@yahoo.com>
2015-12-04 12:40:32 +0100

    Update J2SE and Android matrix engine to use ViewFrustum

--- graphics-by-opengl-android/src/com/nucleus/matrix/android/AndroidMatrixEngine.java
+++ graphics-by-opengl-android/src/com/nucleus/matrix/android/AndroidMatrixEngine.java
@@ -1,8 +1,5 @@
 package com.nucleus.matrix.android;
 
-import android.opengl.Matrix;
-import android.util.Log;
-
 import com.nucleus.camera.ViewFrustum;
 import com.nucleus.renderer.NucleusRenderer.MatrixEngine;
 
@@ -18,25 +15,8 @@ public class AndroidMatrixEngine implements MatrixEngine {
 
     @Override
     public void setProjectionMatrix(ViewFrustum viewFrustum) {
-
-        float[] projection = viewFrustum.getProjection();
-        switch (viewFrustum.getProjectionType()) {
-        case ViewFrustum.PROJECTION_ORTHOGONAL:
-            Matrix.orthoM(viewFrustum.getProjectionMatrix(), 0, projection[ViewFrustum.LEFT_INDEX],
-                    projection[ViewFrustum.RIGHT_INDEX], projection[ViewFrustum.BOTTOM_INDEX],
-                    projection[ViewFrustum.TOP_INDEX], projection[ViewFrustum.NEAR_INDEX],
-                    projection[ViewFrustum.FAR_INDEX]);
-            break;
-        case ViewFrustum.PROJECTION_PERSPECTIVE:
-            Matrix.frustumM(viewFrustum.getProjectionMatrix(), 0, projection[ViewFrustum.LEFT_INDEX],
-                    projection[ViewFrustum.RIGHT_INDEX], projection[ViewFrustum.BOTTOM_INDEX],
-                    projection[ViewFrustum.TOP_INDEX], projection[ViewFrustum.NEAR_INDEX],
-                    projection[ViewFrustum.FAR_INDEX]);
-            break;
-        default:
-            Log.e(ANDROID_MATRIXENGINE_TAG, "Illegal projection: " + viewFrustum.getProjectionType());
-        }
-
+        ViewFrustum.setProjectionMatrix(viewFrustum.getMatrix(), viewFrustum.getProjection(),
+                viewFrustum.getValues());
     }
 
 }
--- graphics-by-opengl-j2se/src/main/java/com/nucleus/matrix/j2se/J2SEMatrixEngine.java
+++ graphics-by-opengl-j2se/src/main/java/com/nucleus/matrix/j2se/J2SEMatrixEngine.java
@@ -2,35 +2,22 @@ package com.nucleus.matrix.j2se;
 
 import com.nucleus.camera.ViewFrustum;
 import com.nucleus.renderer.NucleusRenderer.MatrixEngine;
-import com.nucleus.vecmath.Matrix;
 
+/**
+ * J2SE implementation of the matrix engine.
+ * 
+ * @author Richard Sahlin
+ *
+ */
 public class J2SEMatrixEngine implements MatrixEngine {
 
     public J2SEMatrixEngine() {
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void setProjectionMatrix(ViewFrustum viewFrustum) {
-
-        float[] projection = viewFrustum.getProjection();
-        switch (viewFrustum.getProjectionType()) {
-        case ViewFrustum.PROJECTION_ORTHOGONAL:
-            Matrix.orthoM(viewFrustum.getProjectionMatrix(), 0, projection[ViewFrustum.LEFT_INDEX],
-                    projection[ViewFrustum.RIGHT_INDEX], projection[ViewFrustum.BOTTOM_INDEX],
-                    projection[ViewFrustum.TOP_INDEX], projection[ViewFrustum.NEAR_INDEX],
-                    projection[ViewFrustum.FAR_INDEX]);
-            break;
-        case ViewFrustum.PROJECTION_PERSPECTIVE:
-            Matrix.frustumM(viewFrustum.getProjectionMatrix(), 0, projection[ViewFrustum.LEFT_INDEX],
-                    projection[ViewFrustum.RIGHT_INDEX], projection[ViewFrustum.BOTTOM_INDEX],
-                    projection[ViewFrustum.TOP_INDEX], projection[ViewFrustum.NEAR_INDEX],
-                    projection[ViewFrustum.FAR_INDEX]);
-            break;
-        default:
-            System.err.println("Illegal projection: " + viewFrustum.getProjectionType());
-        }
-
+        ViewFrustum.setProjectionMatrix(viewFrustum.getMatrix(), viewFrustum.getProjection(),
+                viewFrustum.getValues());
     }
 
 }
