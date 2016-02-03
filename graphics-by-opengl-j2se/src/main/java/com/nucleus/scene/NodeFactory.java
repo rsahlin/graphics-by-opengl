7918acf893a84fb35cfffe692887cee6a637fa89
Richard Sahlin <richard_sahlin@yahoo.com>
2016-02-03 11:21:41 +0100

    NodeExporter, remove reference parameter, add documentation

--- graphics-by-opengl-j2se/src/main/java/com/nucleus/scene/NodeFactory.java
+++ graphics-by-opengl-j2se/src/main/java/com/nucleus/scene/NodeFactory.java
@@ -6,13 +6,27 @@ import com.nucleus.renderer.NucleusRenderer;
 
 /**
  * Factory methods for node creation, implements support for new nodes in sublcasses.
+ * This is used by the scene factory implementations to create nodes.
+ * If the node uses Mesh(es) these must be created, allocating buffers, programs and fetching textures as needed.
+ * It shall be possible to render/process the returned node.
  * 
  * @author Richard Sahlin
  *
  */
 public interface NodeFactory {
 
-    public Node create(NucleusRenderer renderer, Node source, String reference, RootNode scene)
+    /**
+     * Creates a new instance of the source node, allocating buffers, programs and fetching textures as needed.
+     * If implementations do not recognize the node to be created they must call super
+     * {@link #create(NucleusRenderer, Node, RootNode)}
+     * 
+     * @param renderer
+     * @param source The source node, the returned node shall be same type.
+     * @param scene Scene data.
+     * @return A new instance of the source node, ready to be rendered/processed
+     * @throws IOException If there is an error fetching shader programs or texture.
+     */
+    public Node create(NucleusRenderer renderer, Node source, RootNode scene)
             throws IOException;
 
 }
