bf4529f443771bf9141c69028a9eccf8d9c53f1d
Richard Sahlin <richard_sahlin@yahoo.com>
2016-04-13 20:26:31 +0200

    Make it possible to affect switch nodes via event

--- graphics-by-opengl-j2se/src/main/java/com/nucleus/scene/NodeController.java
+++ graphics-by-opengl-j2se/src/main/java/com/nucleus/scene/NodeController.java
@@ -1,5 +1,6 @@
 package com.nucleus.scene;
 
+import com.nucleus.properties.EventManager;
 import com.nucleus.properties.EventManager.EventHandler;
 import com.nucleus.properties.Property;
 
@@ -41,9 +42,30 @@ public class NodeController implements EventHandler {
         Property p = Property.create(value);
     }
 
+    /**
+     * Registers this class as a eventhandler for the key, if key is null the {@link #HANDLER_KEY} is used.
+     * NOTE! Only register ONE instance. This shall be called with the
+     * {@link #handleObjectEvent(Object, String, String)} method which will resolve the needed target.
+     * TODO How to make sure only one instance of this class is registered?
+     * 
+     * @param key The key to register this controller for, or null to use default.
+     */
+    public void registerEventHandler(String key) {
+        EventManager.getInstance().registerCategory(key, this);
+    }
+
     @Override
-    public void handleObjectEvent(Object obj, String key, String value) {
-        // TODO Auto-generated method stub
+    public void handleObjectEvent(Object obj, String category, String value) {
+        Actions action = Actions.valueOf(category);
+        Property p = Property.create(value);
+        switch (action) {
+        case SWITCH:
+            SwitchNode node = (SwitchNode) root.getScene().getNodeById(p.getKey());
+            if (node != null) {
+                node.setActive(p.getValue());
+            }
+            break;
+        }
     }
 
     @Override
