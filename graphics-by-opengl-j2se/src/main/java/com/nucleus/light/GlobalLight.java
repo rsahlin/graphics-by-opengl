2f8ea657075f3e7a1719ec80aeb5f08e673dd99d
Richard Sahlin <richard_sahlin@yahoo.com>
2019-03-02 16:56:57 +0100

    Update global light position

--- graphics-by-opengl-j2se/src/main/java/com/nucleus/light/GlobalLight.java
+++ graphics-by-opengl-j2se/src/main/java/com/nucleus/light/GlobalLight.java
@@ -6,7 +6,7 @@ package com.nucleus.light;
  */
 public class GlobalLight {
 
-    private Light light = new DirectionalLight(new float[] { 0, 5000, 10000 }, new float[] { 1, 1, 1 }, 1);
+    private Light light = new DirectionalLight(new float[] { 0, 100000, 500000 }, new float[] { 1, 1, 1 }, 1);
 
     private static GlobalLight globalLight = new GlobalLight();
 
