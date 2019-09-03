69f264edaa6cb09c5f2d294da8152bae2e212356
Richard Sahlin <richard_sahlin@yahoo.com>
2019-09-03 22:39:55 +0200

    Minor fixes for filename search

--- graphics-by-opengl-j2se/src/main/java/com/nucleus/common/FileUtils.java
+++ graphics-by-opengl-j2se/src/main/java/com/nucleus/common/FileUtils.java
@@ -7,81 +7,84 @@ import java.net.URL;
 import java.util.ArrayList;
 
 /**
- * Utilities that are related to filesystem/file operations - singleton class to use ClassLoader when needed.
+ * Utilities that are related to filesystem/file operations - singleton class to
+ * use ClassLoader when needed.
  *
  */
 public class FileUtils {
 
-    protected static FileUtils fileUtils = null;
+	protected static FileUtils fileUtils = null;
 
-    /**
-     * Hide the constructor
-     */
-    private FileUtils() {
-    }
+	/**
+	 * Hide the constructor
+	 */
+	private FileUtils() {
+	}
 
-    public static FileUtils getInstance() {
-        if (fileUtils == null) {
-            fileUtils = new FileUtils();
-        }
-        return fileUtils;
-    }
+	public static FileUtils getInstance() {
+		if (fileUtils == null) {
+			fileUtils = new FileUtils();
+		}
+		return fileUtils;
+	}
 
-    /**
-     * Utility method to return a list with the folder in the specified resource path
-     * 
-     * @param path List of subfolders of this path will be returned
-     * @return folders in the specified path (excluding the path in the returned folder names)
-     */
-    public String[] listResourceFolders(String path) {
-        ClassLoader loader = getClass().getClassLoader();
-        URL url = loader.getResource(path);
-        if (url == null) {
-            return new String[0];
-        }
-        File[] files = new File(url.getFile()).listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.isDirectory();
-            }
-        });
-        // Truncate name so only include the part after specified path.
-        String[] folders = new String[files.length];
-        int index = 0;
-        path = path.replace('/', File.separatorChar);
-        for (File f : files) {
-            int start = f.toString().indexOf(path);
-            folders[index++] = f.toString().substring(start + path.length());
-        }
-        return folders;
-    }
+	/**
+	 * Utility method to return a list with the folder in the specified resource
+	 * path
+	 * 
+	 * @param path List of subfolders of this path will be returned
+	 * @return folders in the specified path (excluding the path in the returned
+	 *         folder names)
+	 */
+	public String[] listResourceFolders(String path) {
+		ClassLoader loader = getClass().getClassLoader();
+		URL url = loader.getResource(path);
+		if (url == null) {
+			return new String[0];
+		}
+		File[] files = new File(url.getFile()).listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.isDirectory();
+			}
+		});
+		// Truncate name so only include the part after specified path.
+		String[] folders = new String[files.length];
+		int index = 0;
+		path = path.replace('/', File.separatorChar);
+		for (File f : files) {
+			int start = f.toString().indexOf(path);
+			folders[index++] = f.toString().substring(start + path.length());
+		}
+		return folders;
+	}
 
-    /**
-     * 
-     * @param path
-     * @param folders
-     * @param mime
-     * @return List of folder/filname for files that ends with mime
-     */
-    public ArrayList<String> listFiles(String path, String[] folders, final String mime) {
-        ArrayList<String> result = new ArrayList<>();
-        ClassLoader loader = getClass().getClassLoader();
-        String comparePath = path.replace('/', File.separatorChar);
-        for (String folder : folders) {
-            URL url = loader.getResource(path + folder);
-            File[] files = new File(url.getFile()).listFiles(new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    return name.endsWith(mime);
-                }
-            });
-            // Truncate name so only include the part after specified path.
-            for (File f : files) {
-                int start = f.toString().indexOf(comparePath);
-                result.add(f.toString().substring(start + comparePath.length()));
-            }
-        }
-        return result;
-    }
+	/**
+	 * 
+	 * @param path
+	 * @param folders
+	 * @param mime
+	 * @return List of folder/filname for files that ends with mime
+	 */
+	public ArrayList<String> listFiles(String path, String[] folders, final String mime) {
+		ArrayList<String> result = new ArrayList<>();
+		ClassLoader loader = getClass().getClassLoader();
+		String comparePath = path.replace('/', File.separatorChar);
+		for (String folder : folders) {
+			URL url = loader.getResource(path + folder);
+			File[] files = new File(url.getFile()).listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.endsWith(mime);
+				}
+			});
+			// Truncate name so only include the part after specified path.
+			for (File f : files) {
+				int start = f.toString().indexOf(comparePath);
+				result.add(f.toString().substring(start + comparePath.length()));
+			}
+		}
+		return result;
+	}
 
 }
--- graphics-by-opengl-j2se/src/main/java/com/nucleus/opengl/GLES30Wrapper.java
+++ graphics-by-opengl-j2se/src/main/java/com/nucleus/opengl/GLES30Wrapper.java
@@ -16,249 +16,255 @@ import com.nucleus.shader.ShaderVariable.VariableType;
  */
 public abstract class GLES30Wrapper extends GLES20Wrapper {
 
-    /**
-     * Implementation constructor - DO NOT USE!!!
-     * TODO - protect/hide this constructor
-     * 
-     * @param platform
-     * @param renderVersion If higher than GLES30, otherwise null
-     */
-    protected GLES30Wrapper(Platform platform, Renderers renderVersion) {
-        super(platform, renderVersion == null ? Renderers.GLES30 : renderVersion);
-    }
+	/**
+	 * Implementation constructor - DO NOT USE!!! TODO - protect/hide this
+	 * constructor
+	 * 
+	 * @param platform
+	 * @param renderVersion If higher than GLES30, otherwise null
+	 */
+	protected GLES30Wrapper(Platform platform, Renderers renderVersion) {
+		super(platform, renderVersion == null ? Renderers.GLES30 : renderVersion);
+	}
 
-    @Override
-    public ProgramInfo getProgramInfo(int program) {
-        int[] activeInfo = new int[3];
-        int[] nameLength = new int[3];
-        glGetProgramiv(program, GLES20.GL_ACTIVE_ATTRIBUTES, activeInfo, VariableType.ATTRIBUTE.index);
-        glGetProgramiv(program, GLES20.GL_ACTIVE_UNIFORMS, activeInfo, VariableType.UNIFORM.index);
-        glGetProgramiv(program, GLES30.GL_ACTIVE_UNIFORM_BLOCKS, activeInfo, VariableType.UNIFORM_BLOCK.index);
-        glGetProgramiv(program, GLES20.GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, nameLength, VariableType.ATTRIBUTE.index);
-        glGetProgramiv(program, GLES20.GL_ACTIVE_UNIFORM_MAX_LENGTH, nameLength, VariableType.UNIFORM.index);
-        glGetProgramiv(program, GLES30.GL_ACTIVE_UNIFORM_BLOCK_MAX_NAME_LENGTH, nameLength,
-                VariableType.UNIFORM_BLOCK.index);
-        return new ProgramInfo(program, activeInfo, nameLength);
-    }
+	@Override
+	public ProgramInfo getProgramInfo(int program) {
+		int[] activeInfo = new int[3];
+		int[] nameLength = new int[3];
+		glGetProgramiv(program, GLES20.GL_ACTIVE_ATTRIBUTES, activeInfo, VariableType.ATTRIBUTE.index);
+		glGetProgramiv(program, GLES20.GL_ACTIVE_UNIFORMS, activeInfo, VariableType.UNIFORM.index);
+		glGetProgramiv(program, GLES30.GL_ACTIVE_UNIFORM_BLOCKS, activeInfo, VariableType.UNIFORM_BLOCK.index);
+		glGetProgramiv(program, GLES20.GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, nameLength, VariableType.ATTRIBUTE.index);
+		glGetProgramiv(program, GLES20.GL_ACTIVE_UNIFORM_MAX_LENGTH, nameLength, VariableType.UNIFORM.index);
+		glGetProgramiv(program, GLES30.GL_ACTIVE_UNIFORM_BLOCK_MAX_NAME_LENGTH, nameLength,
+				VariableType.UNIFORM_BLOCK.index);
+		return new ProgramInfo(program, activeInfo, nameLength);
+	}
 
-    @Override
-    public InterfaceBlock[] getUniformBlocks(ProgramInfo info) {
-        if (info == null || info.getActiveVariables(VariableType.UNIFORM_BLOCK) < 1) {
-            return null;
-        }
-        InterfaceBlock[] uniformBlock = new InterfaceBlock[info.getActiveVariables(VariableType.UNIFORM_BLOCK)];
-        IntBuffer blockInfo = BufferUtils.createIntBuffer(4);
-        for (int i = 0; i < uniformBlock.length; i++) {
-            // GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS
-            int program = info.getProgram();
-            blockInfo.position(InterfaceBlock.ACTIVE_COUNT_INDEX);
-            glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS, blockInfo);
-            if (blockInfo.get(0) > 0) {
-                IntBuffer indices = BufferUtils.createIntBuffer(blockInfo.get(0));
-                glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES,
-                        indices);
-                blockInfo.position(InterfaceBlock.BLOCK_DATA_SIZE_INDEX);
-                glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_DATA_SIZE, blockInfo);
-                blockInfo.position(InterfaceBlock.VERTEX_REFERENCE_INDEX);
-                glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_REFERENCED_BY_VERTEX_SHADER,
-                        blockInfo);
-                blockInfo.position(InterfaceBlock.FRAGMENT_REFERENCE_INDEX);
-                glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_REFERENCED_BY_FRAGMENT_SHADER,
-                        blockInfo);
-                // Block name is fetched using blockIndex so we know the blockIndex to be correct.
-                uniformBlock[i] = new InterfaceBlock(info.getProgram(), i,
-                        glGetActiveUniformBlockName(info.getProgram(), i), blockInfo, indices);
-            }
-        }
-        return uniformBlock;
-    }
+	@Override
+	public InterfaceBlock[] getUniformBlocks(ProgramInfo info) {
+		if (info == null || info.getActiveVariables(VariableType.UNIFORM_BLOCK) < 1) {
+			return null;
+		}
+		InterfaceBlock[] uniformBlock = new InterfaceBlock[info.getActiveVariables(VariableType.UNIFORM_BLOCK)];
+		IntBuffer blockInfo = BufferUtils.createIntBuffer(4);
+		for (int i = 0; i < uniformBlock.length; i++) {
+			// GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS
+			int program = info.getProgram();
+			blockInfo.position(InterfaceBlock.ACTIVE_COUNT_INDEX);
+			glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_ACTIVE_UNIFORMS, blockInfo);
+			if (blockInfo.get(0) > 0) {
+				IntBuffer indices = BufferUtils.createIntBuffer(blockInfo.get(0));
+				glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_ACTIVE_UNIFORM_INDICES, indices);
+				blockInfo.position(InterfaceBlock.BLOCK_DATA_SIZE_INDEX);
+				glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_DATA_SIZE, blockInfo);
+				blockInfo.position(InterfaceBlock.VERTEX_REFERENCE_INDEX);
+				glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_REFERENCED_BY_VERTEX_SHADER, blockInfo);
+				blockInfo.position(InterfaceBlock.FRAGMENT_REFERENCE_INDEX);
+				glGetActiveUniformBlockiv(program, i, GLES30.GL_UNIFORM_BLOCK_REFERENCED_BY_FRAGMENT_SHADER, blockInfo);
+				// Block name is fetched using blockIndex so we know the blockIndex to be
+				// correct.
+				uniformBlock[i] = new InterfaceBlock(info.getProgram(), i,
+						glGetActiveUniformBlockName(info.getProgram(), i), blockInfo, indices);
+			}
+		}
+		return uniformBlock;
+	}
 
-    @Override
-    public NamedShaderVariable getActiveVariable(int program, VariableType type, int index, byte[] nameBuffer)
-            throws GLException {
+	@Override
+	public NamedShaderVariable getActiveVariable(int program, VariableType type, int index, byte[] nameBuffer)
+			throws GLException {
 
-        int[] params = null;
-        int[] indices = new int[] { index };
-        switch (type) {
-            case ATTRIBUTE:
-                return super.getActiveVariable(program, type, index, nameBuffer);
-            case UNIFORM:
-                GLUtils.handleError(this, "UNIFORM_BLOCK clear error");
-                params = new int[NamedShaderVariable.DATA_OFFSET + 1];
-                glGetActiveUniform(program, index, params, NamedShaderVariable.NAME_LENGTH_OFFSET, params,
-                        NamedShaderVariable.SIZE_OFFSET, params, NamedShaderVariable.TYPE_OFFSET, nameBuffer);
-                GLUtils.handleError(this, "UNIFORM glGetActiveUniform for " + new String(nameBuffer));
-                return new NamedShaderVariable(type,
-                        getVariableName(nameBuffer, params[NamedShaderVariable.NAME_LENGTH_OFFSET]), index,
-                        params, 0);
-            case UNIFORM_BLOCK:
-                params = new int[10];
-                glGetActiveUniform(program, index, params, NamedShaderVariable.NAME_LENGTH_OFFSET, params,
-                        NamedShaderVariable.SIZE_OFFSET, params, NamedShaderVariable.TYPE_OFFSET, nameBuffer);
-                GLUtils.handleError(this, "UNIFORM_BLOCK glGetActiveUniforms for " + new String(nameBuffer));
-                glGetActiveUniformsiv(program, 1, indices, 0, GLES30.GL_UNIFORM_BLOCK_INDEX, params,
-                        NamedShaderVariable.BLOCK_INDEX_OFFSET);
-                glGetActiveUniformsiv(program, 1, indices, 0, GLES30.GL_UNIFORM_OFFSET, params,
-                        NamedShaderVariable.DATA_OFFSET);
-                GLUtils.handleError(this, "UNIFORM_BLOCK glGetActiveUniformsiv for " + new String(nameBuffer));
-                // Create shader variable using name excluding [] and .
-                return new NamedShaderVariable(type,
-                        getVariableName(nameBuffer, params[NamedShaderVariable.NAME_LENGTH_OFFSET]), index,
-                        params, 0);
-            default:
-                throw new IllegalArgumentException("Invalid variable type " + type);
+		int[] params = null;
+		int[] indices = new int[] { index };
+		switch (type) {
+		case ATTRIBUTE:
+			return super.getActiveVariable(program, type, index, nameBuffer);
+		case UNIFORM:
+			GLUtils.handleError(this, "UNIFORM_BLOCK clear error");
+			params = new int[NamedShaderVariable.DATA_OFFSET + 1];
+			glGetActiveUniform(program, index, params, NamedShaderVariable.NAME_LENGTH_OFFSET, params,
+					NamedShaderVariable.SIZE_OFFSET, params, NamedShaderVariable.TYPE_OFFSET, nameBuffer);
+			GLUtils.handleError(this, "UNIFORM glGetActiveUniform for " + new String(nameBuffer));
+			return new NamedShaderVariable(type,
+					getVariableName(nameBuffer, params[NamedShaderVariable.NAME_LENGTH_OFFSET]), index, params, 0);
+		case UNIFORM_BLOCK:
+			params = new int[10];
+			glGetActiveUniform(program, index, params, NamedShaderVariable.NAME_LENGTH_OFFSET, params,
+					NamedShaderVariable.SIZE_OFFSET, params, NamedShaderVariable.TYPE_OFFSET, nameBuffer);
+			GLUtils.handleError(this, "UNIFORM_BLOCK glGetActiveUniforms for " + new String(nameBuffer));
+			glGetActiveUniformsiv(program, 1, indices, 0, GLES30.GL_UNIFORM_BLOCK_INDEX, params,
+					NamedShaderVariable.BLOCK_INDEX_OFFSET);
+			glGetActiveUniformsiv(program, 1, indices, 0, GLES30.GL_UNIFORM_OFFSET, params,
+					NamedShaderVariable.DATA_OFFSET);
+			GLUtils.handleError(this, "UNIFORM_BLOCK glGetActiveUniformsiv for " + new String(nameBuffer));
+			// Create shader variable using name excluding [] and .
+			return new NamedShaderVariable(type,
+					getVariableName(nameBuffer, params[NamedShaderVariable.NAME_LENGTH_OFFSET]), index, params, 0);
+		default:
+			throw new IllegalArgumentException("Invalid variable type " + type);
 
-        }
+		}
 
-    }
+	}
 
-    @Override
-    public SLVersion replaceShaderVersion(SLVersion version) {
-        switch (version) {
-            case VERSION100:
-                return version;
-            case VERSION300:
-            case VERSION310:
-            case VERSION320:
-                return platform != Platform.GLES ? SLVersion.VERSION430 : version;
-            case VERSION430:
-                return version;
-            default:
-                throw new IllegalArgumentException("Not implemented for " + version);
-        }
-    }
+	@Override
+	public SLVersion replaceShaderVersion(SLVersion version) {
+		switch (version) {
+		case VERSION100:
+			return version;
+		case VERSION300:
+		case VERSION310:
+		case VERSION320:
+			return platform != Platform.GLES ? SLVersion.VERSION430 : version;
+		case VERSION430:
+		case VERSION450:
+			return version;
+		default:
+			throw new IllegalArgumentException("Not implemented for " + version);
+		}
+	}
 
-    /**
-     * This method works with buffer object, add method with element buffer if needed.
-     * 
-     * @param mode
-     * @param start
-     * @param end
-     * @param count
-     * @param type
-     * @param offset Specifies a byte offset into the buffer bound to GL_ELEMENT_ARRAY_BUFFER to start reading indices
-     * from
-     */
-    public abstract void glDrawRangeElements(int mode, int start, int end, int count, int type, int offset);
+	/**
+	 * This method works with buffer object, add method with element buffer if
+	 * needed.
+	 * 
+	 * @param mode
+	 * @param start
+	 * @param end
+	 * @param count
+	 * @param type
+	 * @param offset Specifies a byte offset into the buffer bound to
+	 *               GL_ELEMENT_ARRAY_BUFFER to start reading indices from
+	 */
+	public abstract void glDrawRangeElements(int mode, int start, int end, int count, int type, int offset);
 
-    /**
-     * Abstraction for glSamplerParameteri( GLuint sampler, GLenum pname, GLint param);
-     * 
-     * @param sampler
-     * @param pname
-     * @param param
-     */
-    public abstract void glSamplerParameteri(int sampler, int pname, int param);
+	/**
+	 * Abstraction for glSamplerParameteri( GLuint sampler, GLenum pname, GLint
+	 * param);
+	 * 
+	 * @param sampler
+	 * @param pname
+	 * @param param
+	 */
+	public abstract void glSamplerParameteri(int sampler, int pname, int param);
 
-    /**
-     * Abstraction for glBindBufferBase(GLenum target, GLuint index, GLuint buffer);
-     * 
-     * @param target
-     * @param index
-     * @param buffer
-     */
-    public abstract void glBindBufferBase(int target, int index, int buffer);
+	/**
+	 * Abstraction for glBindBufferBase(GLenum target, GLuint index, GLuint buffer);
+	 * 
+	 * @param target
+	 * @param index
+	 * @param buffer
+	 */
+	public abstract void glBindBufferBase(int target, int index, int buffer);
 
-    /**
-     * Abstraction for glUniformBlockBinding(GLuint program, GLuint uniformBlockIndex, GLuint uniformBlockBinding);
-     * 
-     * @param program
-     * @param uniformBlockIndex
-     * @param uniformBlockBinding
-     */
-    public abstract void glUniformBlockBinding(int program, int uniformBlockIndex, int uniformBlockBinding);
+	/**
+	 * Abstraction for glUniformBlockBinding(GLuint program, GLuint
+	 * uniformBlockIndex, GLuint uniformBlockBinding);
+	 * 
+	 * @param program
+	 * @param uniformBlockIndex
+	 * @param uniformBlockBinding
+	 */
+	public abstract void glUniformBlockBinding(int program, int uniformBlockIndex, int uniformBlockBinding);
 
-    /**
-     * Abstraction for glBindBufferRange( GLenumtarget, GLuintindex, GLuintbuffer, GLintptroffset, GLsizeiptrsize);
-     * 
-     * @param target
-     * @param index
-     * @param buffer
-     * @param ptroffset
-     * @param ptrsize
-     */
-    public abstract void glBindBufferRange(int target, int index, int buffer, int ptroffset, int ptrsize);
+	/**
+	 * Abstraction for glBindBufferRange( GLenumtarget, GLuintindex, GLuintbuffer,
+	 * GLintptroffset, GLsizeiptrsize);
+	 * 
+	 * @param target
+	 * @param index
+	 * @param buffer
+	 * @param ptroffset
+	 * @param ptrsize
+	 */
+	public abstract void glBindBufferRange(int target, int index, int buffer, int ptroffset, int ptrsize);
 
-    /**
-     * Abstraction for GLuint glGetUniformBlockIndex( GLuint program, const GLchar *uniformBlockName);
-     * 
-     * @param program
-     * @param uniformBlockName
-     * @return
-     */
-    public abstract int glGetUniformBlockIndex(int program, String uniformBlockName);
+	/**
+	 * Abstraction for GLuint glGetUniformBlockIndex( GLuint program, const GLchar
+	 * *uniformBlockName);
+	 * 
+	 * @param program
+	 * @param uniformBlockName
+	 * @return
+	 */
+	public abstract int glGetUniformBlockIndex(int program, String uniformBlockName);
 
-    /**
-     * Astraction for glGetActiveUniformBlockiv(GLuint program, GLuint uniformBlockIndex, GLenum pname, GLint *params);
-     * 
-     * @param program
-     * @param uniformBlockIndex
-     * @param pname
-     * @param buffer
-     */
-    public abstract void glGetActiveUniformBlockiv(int program, int uniformBlockIndex, int pname, IntBuffer buffer);
+	/**
+	 * Astraction for glGetActiveUniformBlockiv(GLuint program, GLuint
+	 * uniformBlockIndex, GLenum pname, GLint *params);
+	 * 
+	 * @param program
+	 * @param uniformBlockIndex
+	 * @param pname
+	 * @param buffer
+	 */
+	public abstract void glGetActiveUniformBlockiv(int program, int uniformBlockIndex, int pname, IntBuffer buffer);
 
-    /**
-     * Abstraction for void glGetActiveUniformBlockName( GLuint program, GLuint uniformBlockIndex, GLsizei bufSize,
-     * GLsizei *length, GLchar *uniformBlockName);
-     * 
-     * @param program
-     * @param uniformBlockIndex
-     * @return
-     */
-    public abstract String glGetActiveUniformBlockName(int program, int uniformBlockIndex);
+	/**
+	 * Abstraction for void glGetActiveUniformBlockName( GLuint program, GLuint
+	 * uniformBlockIndex, GLsizei bufSize, GLsizei *length, GLchar
+	 * *uniformBlockName);
+	 * 
+	 * @param program
+	 * @param uniformBlockIndex
+	 * @return
+	 */
+	public abstract String glGetActiveUniformBlockName(int program, int uniformBlockIndex);
 
-    /**
-     * Abstraction for void glGetActiveUniformsiv( GLuint program, GLsizei uniformCount, const GLuint *uniformIndices,
-     * GLenum pname, GLint *params);
-     * 
-     * @param program
-     * @param uniformCount
-     * @param uniformIndices
-     * @param indicesOffset
-     * @param pname
-     * @param params
-     * @param paramsOffset
-     */
-    public abstract void glGetActiveUniformsiv(int program, int uniformCount, int[] uniformIndices, int indicesOffset,
-            int pname, int[] params, int paramsOffset);
+	/**
+	 * Abstraction for void glGetActiveUniformsiv( GLuint program, GLsizei
+	 * uniformCount, const GLuint *uniformIndices, GLenum pname, GLint *params);
+	 * 
+	 * @param program
+	 * @param uniformCount
+	 * @param uniformIndices
+	 * @param indicesOffset
+	 * @param pname
+	 * @param params
+	 * @param paramsOffset
+	 */
+	public abstract void glGetActiveUniformsiv(int program, int uniformCount, int[] uniformIndices, int indicesOffset,
+			int pname, int[] params, int paramsOffset);
 
-    /**
-     * Abstraction for void *glMapBufferRange( GLenum target, GLintptr offset, GLsizeiptr length, GLbitfield access);
-     * 
-     * @param target
-     * @param offset
-     * @param length
-     * @param access
-     * @return
-     */
-    public abstract ByteBuffer glMapBufferRange(int target, int offset, int length, int access);
+	/**
+	 * Abstraction for void *glMapBufferRange( GLenum target, GLintptr offset,
+	 * GLsizeiptr length, GLbitfield access);
+	 * 
+	 * @param target
+	 * @param offset
+	 * @param length
+	 * @param access
+	 * @return
+	 */
+	public abstract ByteBuffer glMapBufferRange(int target, int offset, int length, int access);
 
-    /**
-     * Abstraction for GLboolean glUnmapBuffer( GLenum target);
-     * 
-     * @param target
-     * @return
-     */
-    public abstract boolean glUnmapBuffer(int target);
+	/**
+	 * Abstraction for GLboolean glUnmapBuffer( GLenum target);
+	 * 
+	 * @param target
+	 * @return
+	 */
+	public abstract boolean glUnmapBuffer(int target);
 
-    /**
-     * Abstraction for void glFlushMappedBufferRange(GLenum target, GLintptr offset, GLsizeiptr length);
-     * 
-     * @param target
-     * @param offset
-     * @param length
-     */
-    public abstract void glFlushMappedBufferRange(int target, int offset, int length);
+	/**
+	 * Abstraction for void glFlushMappedBufferRange(GLenum target, GLintptr offset,
+	 * GLsizeiptr length);
+	 * 
+	 * @param target
+	 * @param offset
+	 * @param length
+	 */
+	public abstract void glFlushMappedBufferRange(int target, int offset, int length);
 
-    /**
-     * 
-     * @param target
-     * @param levels
-     * @param internalformat
-     * @param width
-     * @param height
-     */
-    public abstract void glTexStorage2D(int target, int levels, int internalformat, int width, int height);
+	/**
+	 * 
+	 * @param target
+	 * @param levels
+	 * @param internalformat
+	 * @param width
+	 * @param height
+	 */
+	public abstract void glTexStorage2D(int target, int levels, int internalformat, int width, int height);
 
 }
