7f52f57bd2f4e2fad02f7d1b290eeb69b8bce906
Richard Sahlin <richard_sahlin@yahoo.com>
2019-12-01 21:45:09 +0100

    Fix, use correct SampleCountFlagBits

--- graphics-by-opengl-j2se/src/main/java/com/nucleus/vulkan/structs/PipelineMultisampleStateCreateInfo.java
+++ graphics-by-opengl-j2se/src/main/java/com/nucleus/vulkan/structs/PipelineMultisampleStateCreateInfo.java
@@ -1,12 +1,14 @@
 package com.nucleus.vulkan.structs;
 
+import com.nucleus.vulkan.Vulkan10.SampleCountFlagBits;
+
 /**
  * Wrapper for VkPipelineMultisampleStateCreateInfo
  *
  */
 public class PipelineMultisampleStateCreateInfo {
     // Reserved for future use - VkPipelineMultisampleStateCreateFlags flags;
-    SampleCountFlags rasterizationSamples;
+    SampleCountFlagBits rasterizationSamples;
     boolean sampleShadingEnable;
     float minSampleShading;
     int[] sampleMask;
